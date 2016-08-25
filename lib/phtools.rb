c46a80489cb8a31aceddbf411be76dbe35a6fd46
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2016-08-25 23:40:18 +0300

    fix about message of phtools executable

--- lib/phtools.rb
+++ lib/phtools.rb
@@ -5,7 +5,6 @@ require "phtools/version"
 
 module PhTools
   def self.about
-    %Q{phtools, v#{Phtools::VERSION}, is a bundle of small CLI tools for arranging, renaming, tagging of the photo and video files. Helps to keep your photo-video assets in order.
-    Please run phtools in a terminal via CLI commands: phls, phrename, phbackup, pharrange, phclname, phevent, phfixdate, phfixmd, phmtags, phtagset}
+    %Q{\nphtools v#{PhTools::VERSION} is a bundle of small CLI tools for arranging, renaming, tagging of the photo and video files. Helps to keep your photo-video assets in order. \nPlease run phtools in a terminal via CLI commands: phls, phrename, phbackup, pharrange, phclname, phevent, phfixdate, phfixmd, phmtags, phtagset.\nFor more information run these commands with -h option.}
   end
 end
