8a8a9f2f356e4e3a4e4402a012d72cde9696e7a7
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2016-09-27 21:19:12 +0300

    refactored creators and places

--- lib/phtools/exif_tagger.rb
+++ lib/phtools/exif_tagger.rb
@@ -4,6 +4,4 @@
 
 require 'phtools/exif_tagger/error'
 require 'phtools/exif_tagger/tags'
-require 'phtools/exif_tagger/creators_dir'
-require 'phtools/exif_tagger/places_dir'
 require 'phtools/exif_tagger/tag_collection'
