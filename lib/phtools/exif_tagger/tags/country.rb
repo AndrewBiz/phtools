a8f34ced2495cdc2392d1079cbd437391c776189
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2016-10-23 15:59:07 +0300

    made Country tag

--- lib/phtools/exif_tagger/tags/country.rb
+++ lib/phtools/exif_tagger/tags/country.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 # encoding: UTF-8
 # (c) ANB Andrew Bizyaev
 
@@ -7,23 +8,15 @@ require_relative '_tag_string'
 module ExifTagger
   module Tag
     # MWG:Country, String[0,64]
-    #   = IPTC:Country-PrimaryLocationName XMP-photoshop:Country
-    #       XMP-iptcExt:LocationShownCountryName
+    #  IPTC:Country-PrimaryLocationName, XMP-photoshop:Country, XMP-iptcExt:LocationShownCountryName
     class Country < TagString
       MAX_BYTESIZE = 64
-      EXIFTOOL_TAGS = %w(Country-PrimaryLocationName Country LocationShownCountryName)
-
-      # def initialize(value_raw = '')
-      #   super(value_raw.to_s)
-      # end
+      EXIFTOOL_TAGS = %w(Country-PrimaryLocationName Country LocationShownCountryName).freeze
 
       private
 
       def generate_write_script_lines
-        @write_script_lines = []
-        unless @value.empty?
-          @write_script_lines << %Q(-MWG:Country=#{@value})
-        end
+        @write_script_lines << %(-MWG:Country=#{@value})
       end
     end
   end
--- spec/lib/phtools/exif_tagger/tags/country_spec.rb
+++ spec/lib/phtools/exif_tagger/tags/country_spec.rb
@@ -9,40 +9,72 @@ describe ExifTagger::Tag::Country do
   let(:tag_id) { :country }
   let(:tag_name) { 'Country' }
 
-  let(:val_ok) { 'Russia' }
-  let(:val_orig) { { 'Country' => 'Ukraine' } }
-  let(:val_orig_empty) do {
-    'Country' => '',
-    'Country-PrimaryLocationName' => '',
-    'LocationShownCountryName' => ''
-    }
-  end
+  let(:mhash) { instance_double("MiniExiftool", class: MiniExiftool) }
   let(:tag) { described_class.new(val_ok) }
 
-  it_behaves_like 'any tag'
+  let(:val_ok) { 'Russia' }
+  let(:val_orig) { { 'Country' => 'Ukraine' } }
+  let(:val_orig_empty) { { 'Country-PrimaryLocationName' => '', 'Country' => '', 'LocationShownCountryName' => '' } }
+
+  let(:hash_ok) { { 'Country-PrimaryLocationName' => 'Russia', 'Country' => 'Ukraine', 'LocationShownCountryName' => 'USA' } }
+  let(:hash_last_is_empty) { { 'Country-PrimaryLocationName' => 'Russia', 'Country' => 'Ukraine', 'LocationShownCountryName' => '' } }
+  let(:hash_last_is_all_spaces) { { 'Country-PrimaryLocationName' => 'Russia', 'Country' => 'Ukraine', 'LocationShownCountryName' => '      ' } }
+  let(:hash_last_is_nil) { { 'Country-PrimaryLocationName' => 'Russia', 'Country' => 'Ukraine', 'LocationShownCountryName' => nil } }
+  let(:hash_with_nondefined) { { 'Country-PrimaryLocationName' => 'Russia', 'LocationShownCountryName' => 'USA' } }
+  let(:hash_with_all_bool) { { 'Country-PrimaryLocationName' => true, 'Country' => false, 'LocationShownCountryName' => true } }
+  let(:hash_with_all_empty) { { 'Country-PrimaryLocationName' => '', 'Country' => '', 'LocationShownCountryName' => '' } }
 
   it 'generates write_script for exiftool' do
     expect(tag.to_write_script).to include('-MWG:Country=Russia')
   end
 
-  it_behaves_like 'any paranoid tag'
-
-  context 'when the original value exists' do
-    it 'considers empty strings as a no-value' do
-      tag.check_for_warnings(original_values: val_orig_empty)
-      expect(tag.warnings).to be_empty
-      expect(tag.warnings.inspect).not_to include('has original value:')
-    end
+  it 'does NOT generate write_script for EMPTY value' do
+    tag = described_class.new('')
+    expect(tag.to_write_script).not_to include('-MWG:Country=')
   end
 
-  context 'when gets invalid value' do
-    val_nok = '12345678901234567890123456789012345678901234567890123456789012345' # bytesize=65
-    subject { described_class.new(val_nok) }
-    its(:value) { should be_empty }
-    it { should_not be_valid }
-    its(:value_invalid) { should_not be_empty }
-    its(:value_invalid) { should match_array([val_nok]) }
-    its('errors.inspect') { should include("'#{val_nok}'") }
-    its(:to_write_script) { should be_empty }
+  it_behaves_like 'any tag'
+
+  let(:val_nok_size) { '12345678901234567890123456789012345678901234567890123456789012345' } # bytesize=65
+  it_behaves_like 'any string tag'
+
+  it_behaves_like 'any tag with previous value'
+
+  it_behaves_like 'any tag with MiniExiftool hash input'
+
+  context 'when gets partially defined mini_exiftool hash' do
+    subject { described_class.new(mhash) }
+    it "chooses correct main value = 1st when 1 and 3 exist" do
+      hash = { 'Country-PrimaryLocationName' => 'Russia', 'Country' => '', 'LocationShownCountryName' => 'USA' }
+      allow(mhash).to receive(:[]) { |t| hash[t] }
+
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.value_invalid).to be_empty
+      expect(subject.value).to eq 'Russia'
+      expect(subject.warnings).to be_empty
+    end
+
+    it "chooses correct main value = 2nd when 2 and 3 exist" do
+      hash = { 'Country-PrimaryLocationName' => '', 'Country' => 'Ukraine', 'LocationShownCountryName' => 'USA' }
+      allow(mhash).to receive(:[]) { |t| hash[t] }
+
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.value_invalid).to be_empty
+      expect(subject.value).to eq 'Ukraine'
+      expect(subject.warnings).to be_empty
+    end
+
+    it "chooses correct main value = 3rd when only 3 exists" do
+      hash = { 'Country-PrimaryLocationName' => '', 'Country' => '', 'LocationShownCountryName' => 'USA' }
+      allow(mhash).to receive(:[]) { |t| hash[t] }
+
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.value_invalid).to be_empty
+      expect(subject.value).to eq 'USA'
+      expect(subject.warnings).to be_empty
+    end
   end
 end
