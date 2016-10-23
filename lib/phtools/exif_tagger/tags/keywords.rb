1392847e3fe3401ea8675efa498e3084611b13ef
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2016-10-23 20:05:10 +0300

    finished Keywords tag

--- lib/phtools/exif_tagger/tags/keywords.rb
+++ lib/phtools/exif_tagger/tags/keywords.rb
@@ -1,56 +1,34 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 # encoding: UTF-8
 # (c) ANB Andrew Bizyaev
 
-require_relative '_tag'
+require_relative '_tag_array_of_strings'
 
 module ExifTagger
   module Tag
     # MWG:Keywords, string[0,64]+, List of Strings
-    #   = IPTC:Keywords, XMP-dc:Subject
-    class Keywords < Tag
-      TYPE = :array_of_strings
+    #   IPTC:Keywords, XMP-dc:Subject
+    # exiftool types:
+    #   Keywords = Array ["aaa", "bbb"] OR String "aaa"
+    #   Subject = Array ["aaa", "bbb"] OR String "aaa"
+
+    class Keywords < TagArrayOfStrings
       MAX_BYTESIZE = 64
-      EXIFTOOL_TAGS = %w(Keywords Subject)
+      EXIFTOOL_TAGS = %w(Keywords Subject).freeze
 
-      def initialize(value_raw = [], previous = nil)
-        value = normalize(value_raw)
-        if Tag.empty?(value)
-          super(value)
-        else
-          super(Array(value).flatten.map { |i| i.to_s })
-        end
-      end
+      private
 
-      def check_for_warnings(original_values: {})
+      def validate_vs_previous
         @warnings = []
         @warnings.freeze
       end
 
-      private
-
-      def validate
-        @errors = []
-        @value_invalid = []
-        return if Tag.empty?(@value)
-
-        @value.each do |v|
-          bsize = v.bytesize
-          if bsize > MAX_BYTESIZE
-            @errors << %{#{tag_name}: '#{v}' } +
-                       %{is #{bsize - MAX_BYTESIZE} bytes longer than allowed #{MAX_BYTESIZE}}
-            @value_invalid << v
-          end
-        end
-        @value = @value - @value_invalid
-      end
-
       def generate_write_script_lines
-        @write_script_lines = []
-        unless @value.empty?
-          @value.each do |o|
-            @write_script_lines << %Q(-MWG:Keywords-=#{o})
-            @write_script_lines << %Q(-MWG:Keywords+=#{o})
+        @value.each do |o|
+          unless Tag.empty?(o)
+            @write_script_lines << %(-MWG:Keywords-=#{o})
+            @write_script_lines << %(-MWG:Keywords+=#{o})
           end
         end
       end
--- spec/lib/phtools/exif_tagger/tags/keywords_spec.rb
+++ spec/lib/phtools/exif_tagger/tags/keywords_spec.rb
@@ -9,11 +9,21 @@ describe ExifTagger::Tag::Keywords do
   let(:tag_id) { :keywords }
   let(:tag_name) { 'Keywords' }
 
-  let(:val_ok) { %w(aaa bbb ййй ццц) }
-  let(:val_orig) { { 'Keywords' => %w(original keyword) } }
+  let(:mhash) { instance_double("MiniExiftool", class: MiniExiftool) }
   let(:tag) { described_class.new(val_ok) }
 
-  it_behaves_like 'any tag'
+  let(:val_ok) { %w(aaa bbb ййй ццц) }
+  let(:val_orig) { { 'Keywords' => %w(original keyword) } }
+  let(:val_orig_empty) { { 'Keywords' => '', 'Subject' => [''] } }
+
+  let(:hash_ok) { { 'Keywords' => %w(aaa bbb ййй ццц), 'Subject' => ['s1', 's2'] } }
+  let(:hash_last_is_empty) { { 'Keywords' => %w(aaa bbb ййй ццц), 'Subject' => '' } }
+  let(:hash_last_is_all_spaces) { { 'Keywords' => %w(aaa bbb ййй ццц), 'Subject' => '   ' } }
+  let(:hash_last_is_nil) { { 'Keywords' => %w(aaa bbb ййй ццц), 'Subject' => nil } }
+  let(:hash_with_nondefined) { { 'Keywords' => %w(aaa bbb ййй ццц) } }
+  let(:hash_with_all_bool) { { 'Keywords' => false, 'Subject' => true } }
+  let(:hash_with_all_empty) { { 'Keywords' => '', 'Subject' => '' } }
+
 
   it 'generates write_script for exiftool' do
     expect(tag.to_write_script).to include('-MWG:Keywords-=aaa')
@@ -26,68 +36,120 @@ describe ExifTagger::Tag::Keywords do
     expect(tag.to_write_script).to include('-MWG:Keywords+=ццц')
   end
 
-  context 'when the original value (read by mini_exiftool) exists -' do
-    it 'generates NO warnings' do
-      tag.check_for_warnings(original_values: val_orig)
+  it 'does NOT generate write_script for empty items' do
+    tag = described_class.new(['', nil, ''])
+    expect(tag.to_write_script)
+    expect(tag.write_script_lines.size).to eq 0
+    expect(tag.write_script_lines).not_to include('-MWG:Keywords-=')
+    expect(tag.write_script_lines).not_to include('-MWG:Keywords+=')
+  end
+
+  it 'does NOT generate write_script for empty item only' do
+    tag = described_class.new(['', 'Andrew'])
+    expect(tag.to_write_script)
+    expect(tag.write_script_lines.size).to eq 2
+    expect(tag.write_script_lines).not_to include('-MWG:Keywords-=')
+    expect(tag.write_script_lines).not_to include('-MWG:Keywords+=')
+    expect(tag.write_script_lines).to include('-MWG:Keywords-=Andrew')
+    expect(tag.write_script_lines).to include('-MWG:Keywords+=Andrew')
+  end
+
+  it_behaves_like 'any tag'
+
+  let(:val_ok_size) do
+    val = []
+    val << 'just test string' # bytesize=16
+    val << '1234567890123456789012345678901234567890123456789012345678901234' # bytesize=64
+    val << 'абвгдеёжзийклмнопрстуфхцчшщъыьэю' # bytesize=64
+  end
+
+  let(:val_nok_size) do
+    val = []
+    val << '12345678901234567890123456789012345678901234567890123456789012345' # bytesize=65
+    val << 'абвгдеёжзийклмнопрстуфхцчшщъыьэюZ' # bytesize=65
+    val << 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' # bytesize=66
+  end
+
+  it_behaves_like 'any array_of_strings tag'
+
+  context 'when gets both new and previous value' do
+    let(:previous_value) { described_class.new(mhash) }
+    let(:tag) { described_class.new(val_ok, previous_value) }
+
+    it "ignores previous value of wrong type (!= tag.class)" do
+      tag = described_class.new(val_ok, 'string_class')
+
+      expect(tag.previous).to be_nil
+      expect(tag.value).to eq(val_ok)
+      expect(tag.raw_values).to be_empty
+      expect(tag.to_s).to include(val_ok.to_s)
+      expect(tag).to be_valid
+      expect(tag.errors).to be_empty
+      expect(tag.value_invalid).to be_empty
       expect(tag.warnings).to be_empty
+      expect(tag.warnings.inspect).not_to include('has original value:')
     end
-    it 'generates write_script for exiftool' do
-      tag.check_for_warnings(original_values: val_orig)
-      expect(tag.to_write_script).to include('-MWG:Keywords-=aaa')
-      expect(tag.to_write_script).to include('-MWG:Keywords+=aaa')
-      expect(tag.to_write_script).to include('-MWG:Keywords-=bbb')
-      expect(tag.to_write_script).to include('-MWG:Keywords+=bbb')
-      expect(tag.to_write_script).to include('-MWG:Keywords-=ййй')
-      expect(tag.to_write_script).to include('-MWG:Keywords+=ййй')
-      expect(tag.to_write_script).to include('-MWG:Keywords-=ццц')
-      expect(tag.to_write_script).to include('-MWG:Keywords+=ццц')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords-=aaa')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords+=aaa')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords-=bbb')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords+=bbb')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords-=ййй')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords+=ййй')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords-=ццц')
-      expect(tag.to_write_script).not_to include('# -MWG:Keywords+=ццц')
-      expect(tag.to_write_script).not_to match(/# WARNING: ([\w]*) has original value:/)
+
+    it "generates no warnings if previous.raw_values are empty" do
+      allow(mhash).to receive(:[]) { |tag| val_orig_empty[tag] }
+
+      expect(tag.previous).not_to be_nil
+      expect(tag.previous.class).to eq(described_class)
+      expect(tag.value).to eq(val_ok)
+      expect(tag.raw_values).to be_empty
+      expect(tag.to_s).to include(val_ok.to_s)
+      expect(tag).to be_valid
+      expect(tag.errors).to be_empty
+      expect(tag.value_invalid).to be_empty
+      expect(tag.warnings).to be_empty
+      expect(tag.warnings.inspect).not_to include('has original value:')
+    end
+
+    it "generates NO warnings if previous.raw_values are not empty" do
+      allow(mhash).to receive(:[]) { |tag| val_orig[tag] }
+
+      expect(tag.previous).not_to be_nil
+      expect(tag.previous.class).to eq(described_class)
+      expect(tag.previous.raw_values).not_to be_empty
+      expect(tag.value).to eq(val_ok)
+      expect(tag.raw_values).to be_empty
+      expect(tag).to be_valid
+      expect(tag.errors).to be_empty
+      expect(tag.value_invalid).to be_empty
+      expect(tag.warnings).to be_empty
+      expect(tag.warnings.inspect).not_to include('has original value:')
     end
   end
 
-  context 'when gets a non-flat array as input' do
-    val2 = ['www', ['eee', 'rrr'], 'ttt', [1, [2, 3]]]
-    subject { described_class.new(val2) }
-    val_normal = ['www', 'eee', 'rrr', 'ttt', '1', '2', '3']
-    it 'converts the input to the flat array of strings' do
-      expect(subject.value).to match_array(val_normal)
+  it_behaves_like 'any tag with MiniExiftool hash input'
+
+  context 'when gets partially defined mini_exiftool hash' do
+    subject { described_class.new(mhash) }
+
+    context 'when input values have >1 items in arrays' do
+      it "chooses correct main value = 2nd when only 2 exists" do
+        hash = { 'Keywords' => nil, 'Subject' => ['s1', 's2'] }
+        allow(mhash).to receive(:[]) { |t| hash[t] }
+
+        expect(subject.value).to eq ['s1', 's2']
+        expect(subject).to be_valid
+        expect(subject.errors).to be_empty
+        expect(subject.value_invalid).to be_empty
+        expect(subject.warnings).to be_empty
+      end
     end
-    it { should be_valid }
-    its(:errors) { should be_empty }
-    its(:value_invalid) { should be_empty }
-  end
 
-  context 'when gets invalid value' do
-    val_ok = []
-    val_ok << 'just test string' # bytesize=16
-    val_ok << '1234567890123456789012345678901234567890123456789012345678901234' # bytesize=64
-    val_ok << 'абвгдеёжзийклмнопрстуфхцчшщъыьэю' # bytesize=64
-    val_nok = []
-    val_nok << '12345678901234567890123456789012345678901234567890123456789012345' # bytesize=65
-    val_nok << 'абвгдеёжзийклмнопрстуфхцчшщъыьэюZ' # bytesize=65
-    val_nok << 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' # bytesize=66
+    context 'when input values have 1 item (string)' do
+      it "chooses correct main value = 2nd when only 2 exists" do
+        hash = { 'Keywords' => nil, 'Subject' => 's1' }
+        allow(mhash).to receive(:[]) { |t| hash[t] }
 
-    subject { described_class.new((val_ok + val_nok).sort) }
-    its(:value) { should match_array(val_ok) }
-    it { should_not be_valid }
-    its(:value_invalid) { should_not be_empty }
-    its(:value_invalid) { should match_array(val_nok) }
-
-    val_nok.each do |i|
-      its('errors.inspect') { should include("'#{i}'") }
-      its(:to_write_script) { should_not include("#{i}") }
-    end
-    val_ok.each do |i|
-      its('errors.inspect') { should_not include("'#{i}'") }
-      its(:to_write_script) { should include("#{i}") }
+        expect(subject.value).to eq ['s1']
+        expect(subject).to be_valid
+        expect(subject.errors).to be_empty
+        expect(subject.value_invalid).to be_empty
+        expect(subject.warnings).to be_empty
+      end
     end
   end
 end
