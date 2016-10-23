97e870f67d765de43d4f15ec5a60d30eec4c4f29
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2016-10-23 17:03:43 +0300

    Made ImageUniqueID

--- lib/phtools/exif_tagger/tags/image_unique_id.rb
+++ lib/phtools/exif_tagger/tags/image_unique_id.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 # encoding: UTF-8
 # (c) ANB Andrew Bizyaev
 
@@ -9,28 +10,22 @@ module ExifTagger
     # ImageUniqueID, String
     class ImageUniqueId < TagString
       MAX_BYTESIZE = 32
-      EXIFTOOL_TAGS = %w(ImageUniqueID)
+      EXIFTOOL_TAGS = %w(ImageUniqueID).freeze
 
-      # def initialize(value_raw = [])
-      #   super(value_raw.to_s)
-      # end
+      private
 
-      def check_for_warnings(original_values: {})
+      def validate_vs_previous
         @warnings = []
-        v = original_values[EXIFTOOL_TAGS[0]]
-        if v =~ /(\d{8}-\S+)/
-          @warnings << "#{tag_name} has original correct value: '#{v}'"
+        return if @previous.nil?
+        val = @previous.raw_values[EXIFTOOL_TAGS[0]]
+        if val =~ /(\d{8}-\S+)/
+          @warnings << "#{tag_name} has original value: '#{val}'"
         end
         @warnings.freeze
       end
 
-      private
-
       def generate_write_script_lines
-        @write_script_lines = []
-        unless @value.empty?
-          @write_script_lines << %Q(-ImageUniqueID=#{@value})
-        end
+        @write_script_lines << %(-ImageUniqueID=#{@value})
       end
     end
   end
