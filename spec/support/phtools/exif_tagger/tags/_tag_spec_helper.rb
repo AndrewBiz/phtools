55a8b49b83d47f77f9b6f41f7a54ea4cb6e49c17
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2016-11-01 22:37:14 +0300

    clean unused tests

--- spec/support/phtools/exif_tagger/tags/_tag_spec_helper.rb
+++ spec/support/phtools/exif_tagger/tags/_tag_spec_helper.rb
@@ -289,31 +289,3 @@ shared_examples_for 'any tag who cares about previous value' do
     end
   end
 end
-
-# TODO: to remove paranoid tag
-# *****************************************************************************#
-shared_examples_for 'any paranoid tag' do
-  context 'when the original value exists' do
-    it 'generates warnings' do
-      tag.check_for_warnings(original_values: val_orig)
-      expect(tag.warnings).not_to be_empty
-      expect(tag.warnings.inspect).to include('has original value:')
-    end
-    context 'AND force_write is set to FALSE' do
-      it 'generates write_script with WARNINGs and _commented_ lines' do
-        tag.check_for_warnings(original_values: val_orig)
-        tag.force_write = false
-        expect(tag.to_write_script).to include('# -')
-        expect(tag.to_write_script).to match(/# WARNING: ([\w]*) has original value:/)
-      end
-    end
-    context 'AND force_write is set to TRUE' do
-      it 'generates write_script with WARNINGs and _UNcommented_ lines' do
-        tag.check_for_warnings(original_values: val_orig)
-        tag.force_write = true
-        expect(tag.to_write_script).not_to include('# -')
-        expect(tag.to_write_script).to match(/# WARNING: ([\w]*) has original value:/)
-      end
-    end
-  end
-end
