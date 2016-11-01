349277f8c8cbd5a1e59656a8ff99441865e6e485
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2016-11-01 22:22:25 +0300

    Finished DateTimeOriginal tag

--- lib/phtools/exif_tagger/tags/date_time_original.rb
+++ lib/phtools/exif_tagger/tags/date_time_original.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 # encoding: UTF-8
 # (c) ANB Andrew Bizyaev
 
@@ -10,20 +11,26 @@ module ExifTagger
     #    EXIF:DateTimeOriginal, (EXIF:SubSecTimeOriginal), IPTC:DateCreated + IPTC:TimeCreated,    XMP-photoshop:DateCreated
     # creation date of the intellectual content being shown
     class DateTimeOriginal < TagDate
-      TYPE = :date_time
       MAX_BYTESIZE = 32
-      EXIFTOOL_TAGS = %w(DateTimeOriginal SubSecTimeOriginal DateCreated TimeCreated)
+      EXIFTOOL_TAGS = %w(DateTimeOriginal SubSecTimeOriginal DateCreated TimeCreated).freeze
 
       private
 
-      def generate_write_script_lines
-        @write_script_lines = []
-        case
-        when @value.kind_of?(String) && !@value.empty?
-          @write_script_lines << %Q(-MWG:DateTimeOriginal=#{@value})
-        when @value.kind_of?(DateTime) || @value.kind_of?(Time)
-          @write_script_lines << %Q(-MWG:DateTimeOriginal=#{@value.strftime('%F %T')})
+      def get_from_raw
+        return @raw_values['DateTimeOriginal'] unless Tag.empty?(@raw_values['DateTimeOriginal'])
+        unless Tag.empty?(@raw_values['DateCreated'])
+          return @raw_values['DateCreated'] if @raw_values['DateCreated'].is_a?(DateTime)
+          return make_date_from(@raw_values['DateCreated'], @raw_values['TimeCreated']) if @raw_values['DateCreated'].is_a?(String)
         end
+        EMPTY
+      end
+
+      def generate_write_script_lines
+        @write_script_lines << if @value.is_a?(DateTime)
+                                 %(-MWG:DateTimeOriginal=#{@value.strftime('%F %T')})
+                               else
+                                 %(-MWG:DateTimeOriginal=#{@value})
+                               end
       end
     end
   end
--- spec/lib/phtools/exif_tagger/tags/date_time_original_spec.rb
+++ spec/lib/phtools/exif_tagger/tags/date_time_original_spec.rb
@@ -10,18 +10,84 @@ describe ExifTagger::Tag::DateTimeOriginal do
   let(:tag_id) { :date_time_original }
   let(:tag_name) { 'DateTimeOriginal' }
 
-  let(:val_ok) { DateTime.new(2014, 07, 31, 21, 1, 1) }
-  let(:val_orig) { { 'DateTimeOriginal' => DateTime.new(2014, 01, 21, 11, 5, 5) } }
+  let(:mhash) { instance_double("MiniExiftool", class: MiniExiftool) }
   let(:tag) { described_class.new(val_ok) }
 
-  let(:val_nok_size) { '123456789012345678901234567890123' } # bytesize=33
-  it_behaves_like 'any tag'
+  let(:val_ok) { DateTime.new(2014, 1, 21, 11, 5, 5) }
+  let(:val_orig) { { 'DateTimeOriginal' => DateTime.new(2014, 01, 21, 11, 5, 5), 'SubSecTimeOriginal' => 123, 'DateCreated' => '2005:05:05', 'TimeCreated' => '05:05:05+05:00' } }
+  let(:val_orig_empty) { { 'DateTimeOriginal' => '', 'SubSecTimeOriginal' => '', 'DateCreated' => '', 'TimeCreated' => '' } }
+
+  let(:hash_ok) { { 'DateTimeOriginal' => DateTime.new(2014, 01, 21, 11, 5, 5), 'SubSecTimeOriginal' => 123, 'DateCreated' => '2005:05:05', 'TimeCreated' => '05:05:05+05:00' } }
+  let(:hash_last_is_empty) { { 'DateTimeOriginal' => DateTime.new(2014, 01, 21, 11, 5, 5), 'SubSecTimeOriginal' => 123, 'DateCreated' => '2005:05:05', 'TimeCreated' => '' } }
+  let(:hash_last_is_all_spaces) { { 'DateTimeOriginal' => DateTime.new(2014, 01, 21, 11, 5, 5), 'SubSecTimeOriginal' => 123, 'DateCreated' => '2005:05:05', 'TimeCreated' => '     ' } }
+  let(:hash_last_is_nil) { { 'DateTimeOriginal' => DateTime.new(2014, 01, 21, 11, 5, 5), 'SubSecTimeOriginal' => 123, 'DateCreated' => '2005:05:05', 'TimeCreated' => nil } }
+  let(:hash_with_nondefined) { { 'DateTimeOriginal' => DateTime.new(2014, 01, 21, 11, 5, 5), 'DateCreated' => '2005:05:05', 'TimeCreated' => '05:05:05+05:00' } }
+  let(:hash_with_all_bool) { { 'DateTimeOriginal' => false, 'SubSecTimeOriginal' => true, 'DateCreated' => false, 'TimeCreated' => false } }
+  let(:hash_with_all_empty) { { 'DateTimeOriginal' => '', 'SubSecTimeOriginal' => nil, 'DateCreated' => '', 'TimeCreated' => '' } }
 
   it 'generates write_script for exiftool' do
-    expect(tag.to_write_script).to include('-MWG:DateTimeOriginal=2014-07-31 21:01:01')
+    expect(tag.to_write_script).to include('-MWG:DateTimeOriginal=2014-01-21 11:05:05')
   end
 
-  it_behaves_like 'any paranoid tag'
+  it 'does NOT generate write_script for EMPTY value' do
+    tag = described_class.new('')
+    expect(tag.to_write_script).not_to include('-MWG:DateTimeOriginal=')
+  end
 
+  it_behaves_like 'any tag'
+
+  let(:val_nok_size) { '123456789012345678901234567890123' } # bytesize=33
   it_behaves_like 'any date-tag'
+
+  it_behaves_like 'any tag who cares about previous value'
+
+  it_behaves_like 'any tag with MiniExiftool hash input'
+
+  context 'when gets partially defined mini_exiftool hash' do
+    subject { described_class.new(mhash) }
+    it "chooses correct main value with DateCreated (DateTime)" do
+      hash = { 'DateTimeOriginal' => '', 'SubSecTimeOriginal' => '123', 'DateCreated' => DateTime.new(2016, 11, 01, 21, 53, 3), 'TimeCreated' => '05:05:05+05:00' }
+      allow(mhash).to receive(:[]) { |tag| hash[tag] }
+
+      expect(subject.raw_values['DateTimeOriginal']).to be_empty
+      expect(subject.raw_values['SubSecTimeOriginal']).to eq '123'
+      expect(subject.raw_values['DateCreated']).to eq DateTime.new(2016, 11, 01, 21, 53, 3)
+      expect(subject.raw_values['TimeCreated']).to eq '05:05:05+05:00'
+      expect(subject.value).to eq DateTime.new(2016, 11, 01, 21, 53, 3)
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.value_invalid).to be_empty
+      expect(subject.warnings).to be_empty
+    end
+
+    it "chooses correct main value with DateCreated + TimeCreated (String)" do
+      hash = { 'DateTimeOriginal' => '', 'SubSecTimeOriginal' => '123', 'DateCreated' => '2005:05:05', 'TimeCreated' => '05:05:05+05:00' }
+      allow(mhash).to receive(:[]) { |tag| hash[tag] }
+
+      expect(subject.raw_values['DateTimeOriginal']).to be_empty
+      expect(subject.raw_values['SubSecTimeOriginal']).to eq '123'
+      expect(subject.raw_values['DateCreated']).to eq '2005:05:05'
+      expect(subject.raw_values['TimeCreated']).to eq '05:05:05+05:00'
+      expect(subject.value).to eq DateTime.new(2005, 5, 5, 5, 5, 5, '+05:00')
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.value_invalid).to be_empty
+      expect(subject.warnings).to be_empty
+    end
+
+    it "avoids execptions when parsing incorrect date from 2 part strings" do
+      hash = { 'DateTimeOriginal' => '', 'SubSecTimeOriginal' => '123', 'DateCreated' => 'unknown date', 'TimeCreated' => 'time' }
+      allow(mhash).to receive(:[]) { |tag| hash[tag] }
+
+      expect(subject.raw_values['DateTimeOriginal']).to be_empty
+      expect(subject.raw_values['SubSecTimeOriginal']).to eq '123'
+      expect(subject.raw_values['DateCreated']).to eq 'unknown date'
+      expect(subject.raw_values['TimeCreated']).to eq 'time'
+      expect(subject.value).to be_empty
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.value_invalid).to be_empty
+      expect(subject.warnings).to be_empty
+    end
+  end
 end
