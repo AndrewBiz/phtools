ffc0083f71ac4ae53a4bc68c1229019a36f08b77
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2017-10-29 20:05:00 +0300

    ruby version now > 2.4.0

--- lib/phtools/utils/ruby_version.rb
+++ lib/phtools/utils/ruby_version.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 # encoding: UTF-8
+
 # (c) ANB Andrew Bizyaev
 
-RUBY_VERSION_WANTED = '2.0.0'
+RUBY_VERSION_WANTED = '2.4.0'
 
 begin
   fail "Ruby version must be >= #{RUBY_VERSION_WANTED}" if
     RUBY_VERSION < RUBY_VERSION_WANTED
-rescue => e
+rescue StandardError => e
   STDERR.puts e.message
   exit 1
 end
